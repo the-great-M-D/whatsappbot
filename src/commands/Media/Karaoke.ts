aa04fcc1af54c387b641380927b819f641fd0898
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-19 01:38:16 +0200

    Update Karaoke.ts

--- src/commands/Media/Karaoke.ts
+++ src/commands/Media/Karaoke.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         const term = joined.trim()
         const { videos } = await yts(term + ' karaoke song')
         if (!videos || videos.length <= 0) return void M.reply(`No Matching videos found for the term *${term}*`)
-        const text = `Via Kaoi👾 && Void`
+        const text = `Via M_D 🤹 && Void`
 
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
@@ -30,7 +30,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `👾 Handcrafted for you by Kaoi 👾`,
+                        body: `🤹 Handcrafted for you by M_D 🤹`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
