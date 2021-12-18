fc5366b9d85d974c020db7ac422e960b556eec01
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-19 00:49:24 +0200

    Update YTAudio.ts

--- src/commands/Media/YTAudio.ts
+++ src/commands/Media/YTAudio.ts
@@ -27,22 +27,3 @@ export default class Command extends BaseCommand {
         )
     }
 }
-export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'command_goes_here',
-            description: 'command description',
-            category: 'category',
-            usage: `${client.config.prefix}command`
-        })
-    }
-
-    //eslint-disable-next-line
-    run = async (M: ISimplifiedMessage, args: IParsedArgs): Promise<void> => {
-        if (!M.urls.length) {
-            return void M.reply('🔎 Provide An IP Address'   
-        }
-         
-    
-    }
-} 
