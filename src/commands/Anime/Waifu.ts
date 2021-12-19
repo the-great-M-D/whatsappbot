ca27281ceca65ce2087337e5920abe726d7abcde
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-19 02:00:34 +0200

    Update Waifu.ts

--- src/commands/Anime/Waifu.ts
+++ src/commands/Anime/Waifu.ts
@@ -53,48 +53,6 @@ export default class Command extends BaseCommand {
 
 
 
-export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'nued',
-            description: `sends you  the Waifus aka\nGirls you can't have.`,
-            aliases: ['animegirl'],
-            category: 'anime',
-            usage: `${client.config.prefix}nued`,
-            baseXp: 50
-        })
-    }
-
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        // fetch result of https://waifu.pics/api/nsfw/waifu from the API using axios
-        const { data } = await axios.get('https://waifu.pics/api/nsfw/waifu')
-        const buffer = await request.buffer(data.url).catch((e) => {
-            return void M.reply(e.message)
-        })
-        while (true) {
-            try {
-                M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,
-                    undefined,
-                    undefined,
-                    `More than one waifu, will ruin your laifu.\n`,
-                    undefined
-                ).catch((e) => {
-                    console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
-                    // console.log('Failed')
-                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
-                })
-                break
-            } catch (e) {
-                // console.log('Failed2')
-                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
-                console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
-            }
-        }
-        return void null
-    }
-}
 
 
 
