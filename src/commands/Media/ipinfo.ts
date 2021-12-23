3dc0d6ab149418a05002a400e5d47924cfd72e29
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-24 00:14:12 +0200

    Update ipinfo.ts

--- src/commands/Media/ipinfo.ts
+++ src/commands/Media/ipinfo.ts
@@ -23,9 +23,10 @@ export default class Command extends BaseCommand {
                 `https://ipinfo.io/${term}/json`
             )
             .then((res) => {
-                if (res.status !== 200) return void M.reply(`🔍 Error: ${res.status}`)
-                // return void M.reply(`🔍Command Used : ${Command.count} times\n Result for *${term}*\n\n\n ${result}`)
-                    respons = res.data
+            
+               
+                
+                  const respons = res.data
                 return void M.reply(`🔍 Result for *${term}*\n\n\n ${respons}`)
             })
             .catch((err) => {
