f5e32a8f597bed070d8c52154d268c1efd0b026e
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-24 02:12:10 +0200

    Create boom.ts

---
+++ src/commands/Dev/boom.ts
@@ -0,0 +1,36 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+
+import BaseCommand from '../../lib/BaseCommand'
+
+import WAClient from '../../lib/WAClient'
+
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+
+    constructor(client: WAClient, handler: MessageHandler) {
+
+        super(client, handler, {
+
+            command: 'boom',
+
+            description: 'Generally used to say hie 🤗',
+
+            category: 'Dev',
+
+            usage: `${client.config.prefix}boom`,
+
+            baseXp: 0
+
+        })
+
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+      for(let i = 0; i > 5; i++){
+
+        return void (await M.reply(`🤹‍♂️ Ey Mother Fucker 🥃 ${M.sender.username}!`))}
+
+    }
+
+}
