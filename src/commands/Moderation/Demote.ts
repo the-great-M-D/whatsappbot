c56dce4a6b35a8d9e7e467fd766ab273e7833335
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-27 05:34:03 +0200

    Update Demote.ts

--- src/commands/Moderation/Demote.ts
+++ src/commands/Moderation/Demote.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            adminOnly: true,
+            
             command: 'demote',
             description: 'demotes the mentioned users',
             category: 'moderation',
