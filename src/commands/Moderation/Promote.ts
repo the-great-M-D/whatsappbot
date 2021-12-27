d4335b3d72a971801079fa369f7d099ee01e99e8
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-27 05:30:21 +0200

    Update Promote.ts

--- src/commands/Moderation/Promote.ts
+++ src/commands/Moderation/Promote.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            adminOnly: true,
+            adminOnly: false,
             command: 'promote',
             description: 'promotes the mentioned users',
             category: 'moderation',
