d22e750a9292da6eddb97811dcbb3df3b25bd111
M_D <93617862+the-great-M-D@users.noreply.github.com>
2021-12-30 19:00:37 +0200

    Update mtn.ts

--- src/commands/Files/mtn.ts
+++ src/commands/Files/mtn.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
         })
     }
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        const { data } = await axios.get('https://github.com/the-great-M-D/hcdecryptor/blob/master/MTN%20VPS.hc')
+        const { data } = await axios.get('https://raw.githubusercontent.com/the-great-M-D/hcdecryptor/master/MTN%20VPS.hc')
         const buffer = await request.buffer(data.url).catch((e) => {
             return void M.reply(e.message)
         })
