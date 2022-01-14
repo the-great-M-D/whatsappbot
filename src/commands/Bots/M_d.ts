036c7960988fe7280e4953cc7767ecb7178d7fca
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-01-14 23:31:50 +0200

    Update M_d.ts

--- src/commands/Bots/M_d.ts
+++ src/commands/Bots/M_d.ts
@@ -2,7 +2,7 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../typings'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
