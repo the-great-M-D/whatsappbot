0f6dd00a3984b938e9ff029d1a8e4f8b69517d6f
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-01-14 10:24:24 +0200

    Update Kaoi.ts

--- src/commands/Kaoi.ts
+++ src/commands/Kaoi.ts
@@ -1,3 +1,7 @@
+import { config } from 'dotenv'
+
+config()
+
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
