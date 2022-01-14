13a4ab5fce2d69d3661bae77c17fe5773f48301c
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-01-14 21:50:49 +0200

    Create M_D.ts

---
+++ src/commands/Bots/M_D.ts
@@ -0,0 +1,23 @@
+
+import MessageHandler from '../Handlers/MessageHandler'
+import BaseCommand from '../lib/BaseCommand'
+import WAClient from '../lib/WAClient'
+import { ISimplifiedMessage } from '../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'm_d',
+            description: 'Displays the info about M-D Bot',
+            category: 'bots',
+            usage: `${client.config.prefix}info`,
+            baseXp: 200
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void M.reply(
+            `🤹 *M_D*🤹\n\n🍀 *Description:* The Great M_D WhatsApp Bot \n\n🤹 *Prefix:* Use !command-here \n\n🤹 *Example:* !help \n\n🌐 *URL:* https://https://github.com/the-great-M-D \n`
+        ).catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
+    }
+}
