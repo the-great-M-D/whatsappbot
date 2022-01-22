8ff85ec3b492956bd74ff363a1630fcf44333c52
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-01-22 13:30:21 +0200

    Update Revoke.ts

--- src/commands/Moderation/Revoke.ts
+++ src/commands/Moderation/Revoke.ts
@@ -21,6 +21,6 @@ export default class Command extends BaseCommand {
         await this.client.revokeInvite(M.from).catch(() => {
             return void M.reply('Failed to revoke the group link')
         })
-        return void M.reply('Group link revoked')
+        return void M.reply('Group link revoked 🤹‍♂️')
     }
 }
