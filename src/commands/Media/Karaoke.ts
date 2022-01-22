c06ab512d03952643486a4a03af6fb649176bf41
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-01-22 13:13:38 +0200

    Update Karaoke.ts

--- src/commands/Media/Karaoke.ts
+++ src/commands/Media/Karaoke.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         const term = joined.trim()
         const { videos } = await yts(term + ' karaoke song')
         if (!videos || videos.length <= 0) return void M.reply(`No Matching videos found for the term *${term}*`)
-        const text = `Via M_D 🤹 & Codding Family`
+        const text = `The great M_D 🤹 & Coding Family 🤹‍♂️`
 
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
