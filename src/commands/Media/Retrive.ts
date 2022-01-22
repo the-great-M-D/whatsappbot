6cdbd74d04077e44981a9503a0612eb0da5c7802
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-01-22 13:19:21 +0200

    Update Retrive.ts

--- src/commands/Media/Retrive.ts
+++ src/commands/Media/Retrive.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
             ],
             undefined,
             undefined,
-            'Hippity Hoppity, this message is now public property...M_D🤹'
+            'Hippity Hoppity, this message is now public property... M_D Bot 🤹'
         )
     }
 }
