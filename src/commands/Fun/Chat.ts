f03dc0289f7b674ef34f49d46c20ea77f3800bb3
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-02-13 08:57:20 +0200

    Update Chat.ts

--- src/commands/Fun/Chat.ts
+++ src/commands/Fun/Chat.ts
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
                     return void M.reply(res.data.cnt)
                 })
                 .catch(() => {
-                    M.reply(`Intriguing...`)
+                    M.reply(`use !chat then say something to the Bot here 😌`)
                 })
         } else {
             M.reply(`Chat Bot Url not set\nRefer to ${this.client.config.prefix}guide to get Chat Bot Url`)
