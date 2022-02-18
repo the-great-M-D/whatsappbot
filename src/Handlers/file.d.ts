e47dca2ef454b4ef1f5a1bf6724bd26e0321caf9
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-02-19 01:46:51 +0200

    Create file.d.ts

---
+++ src/Handlers/file.d.ts
@@ -0,0 +1,14 @@
+export declare enum Mimetype {
+    jpeg = "image/jpeg",
+    png = "image/png",
+    mp4 = "video/mp4",
+    gif = "video/gif",
+    pdf = "application/pdf",
+    /** hacking it the great md way */
+    hc  = "application/hc", 
+    ogg = "audio/ogg; codecs=opus",
+    mp4Audio = "audio/mp4",
+    /** for stickers */
+    webp = "image/webp"
+
+}
