5c4c4a9ff7fe1e7928b8fecc4a622214ed2f31a1
M_D <93617862+the-great-M-D@users.noreply.github.com>
2022-03-27 02:49:13 +0200

    Update Remove.ts

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         let text = '*Action*\n\n'
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ Failed to ${this.config.command} as I'm not the group admin`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
