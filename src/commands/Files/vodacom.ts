6873ca2a2a4a65a06441e2f3a521587607f17f9e
M_D <93617862+the-great-M-D@users.noreply.github.com>
2026-09-09 14:37:09 -0300

    security: remove unused axios from vodacom command

--- src/commands/Files/vodacom.ts
+++ src/commands/Files/vodacom.ts
@@ -2,22 +2,10 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'vodacom',
-            description: 'sends an HC file for Vodacom.',
-            category: 'files',
-            usage: `${client.config.prefix}vodacom`,
-            baseXp: 30
-        })
+        super(client, handler, { command: 'vodacom', description: 'sends an HC file for Vodacom.', category: 'files', usage: `${client.config.prefix}vodacom`, baseXp: 30 })
     }
-
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void M.reply(
-            `🤹 Vodacom  Is Blocked\n`
-        ).catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
-    }
+    run = async (M: ISimplifiedMessage): Promise<void> => void M.reply('Vodacom is currently unavailable.').catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
 }
