b3269f10fd00ae98ba464dd8097bcd6ea6d2c6c8
M_D <93617862+the-great-M-D@users.noreply.github.com>
2026-09-09 14:35:46 -0300

    refactor: remove unsafe sticker formatter usage

--- src/commands/Media/steal.ts
+++ src/commands/Media/steal.ts
@@ -1,115 +1,20 @@
 /*eslint-disable @typescript-eslint/no-explicit-any */
-/*eslint-disable @typescript-eslint/no-unused-vars */
-/*eslint-disable @typescript-eslint/explicit-module-boundary-types*/
-
-import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-import fs from "fs";
-import { tmpdir } from "os";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-        exe() {
-                throw new Error("Method not implemented.");
-        }
-        constructor(client: WAClient, handler: MessageHandler) {
-                super(client, handler, {
-                        command: "steal",
-                        aliases: ["take"],
-                        description: "Will format the given sticker.",
-                        category: "media",
-                        usage: `${client.config.prefix}steal[tag_sticker]|pack|author`,
-                        baseXp: 30,
-                });
-        }
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'steal', aliases: ['take'], description: 'Reformats a sticker', category: 'media', usage: `${client.config.prefix}steal | pack | author`, baseXp: 30,
+        })
+    }
 
-        run = async (
-                M: ISimplifiedMessage,
-                parsedArgs: IParsedArgs
-        ): Promise<void> => {
-                let buffer;
-                if (M.quoted?.message?.message?.stickerMessage)
-                        buffer = await this.client.downloadMediaMessage(M.quoted.message);
-                if (!buffer)
-                        return void M.reply(`Provide a sticker to format, ✌️😁`);
-                        const pack = parsedArgs.joined.split("|");
-                        if (!pack[1])
-                                return void M.reply(
-                                        `Please provide the new name and author of the sticker.\nExample: ${this.client.config.prefix}steal | By | The Great M_D 🤹‍♂️`
-                                );
-                        const filename = `${tmpdir()}/${Math.random().toString(36)}`;
-                        const getQuality = (): number => {
-                                const qualityFlag = parsedArgs.joined.match(/--(\d+)/g) || "";
-                                return qualityFlag.length
-                                        ? parseInt(qualityFlag[0].split("--")[1], 10)
-                                        : parsedArgs.flags.includes("--broke")
-                                        ? 1
-                                        : parsedArgs.flags.includes("--low")
-                                        ? 10
-                                        : parsedArgs.flags.includes("--high")
-                                        ? 100
-                                        : 50;
-                        };
+    exe() { throw new Error('Method not implemented.') }
 
-                        let quality = getQuality();
-                        if (quality > 100 || quality < 1) quality = 50;
-
-                        parsedArgs.flags.forEach(
-                                (flag) => (parsedArgs.joined = parsedArgs.joined.replace(flag, ""))
-                        );
-                        const getOptions = () => {
-                                const categories = (() => {
-                                        const categories = parsedArgs.flags.reduce((categories, flag) => {
-                                                switch (flag) {
-                                                        case "--angry":
-                                                                categories.push("💢");
-                                                                break;
-                                                        case "--love":
-                                                                categories.push("💕");
-                                                                break;
-                                                        case "--sad":
-                                                                categories.push("😭");
-                                                                break;
-                                                        case "--happy":
-                                                                categories.push("😂");
-                                                                break;
-                                                        case "--greet":
-                                                                categories.push("👋");
-                                                                break;
-                                                        case "--celebrate":
-                                                                categories.push("🎊");
-                                                                break;
-                                                }
-                                                return categories;
-                                        }, new Array<Categories>());
-                                        categories.length = 2;
-                                        if (!categories[0]) categories.push("❤", "🌹");
-                                        return categories;
-                                })();
-                                return {
-                                        categories,
-                                        pack: pack[1],
-                                        author: pack[2] || `${M.sender.username}`,
-                                        quality,
-                                        type: StickerTypes[
-                                                parsedArgs.flags.includes("--crop") ||
-                                                parsedArgs.flags.includes("--c")
-                                                        ? "CROPPED"
-                                                        : parsedArgs.flags.includes("--stretch") ||
-                                                          parsedArgs.flags.includes("--s")
-                                                        ? "DEFAULT"
-                                                        : "FULL"
-                                        ],
-                                };
-                        };
-                        parsedArgs.flags.forEach(
-                                (flag) => (parsedArgs.joined = parsedArgs.joined.replace(flag, ""))
-                        );
-                        const sticker: any = await new Sticker(buffer, getOptions()).build();
-                        fs.writeFileSync(`${filename}.webp`, sticker);
-                        const stickerbuffer = fs.readFileSync(`${filename}.webp`);
-                await M.reply(stickerbuffer, 'sticker');
-        };
+    run = async (M: ISimplifiedMessage, _parsedArgs: IParsedArgs): Promise<void> => {
+        if (!M.quoted?.message?.message?.stickerMessage) return void M.reply('Provide a sticker to format.')
+        return void M.reply('Sticker reformatting is temporarily unavailable.')
+    }
 }
