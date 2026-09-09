93a192069d3f7af977e88ab0a12477b4e87a7ebc
M_D <93617862+the-great-M-D@users.noreply.github.com>
2026-09-09 14:36:45 -0300

    security: remove unused axios from telkom command

--- src/commands/Files/telkom.ts
+++ src/commands/Files/telkom.ts
@@ -2,22 +2,10 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'telkom',
-            description: 'sends an HC file for Telkom.',
-            category: 'files',
-            usage: `${client.config.prefix}telkom`,
-            baseXp: 30
-        })
+        super(client, handler, { command: 'telkom', description: 'sends an HC file for Telkom.', category: 'files', usage: `${client.config.prefix}telkom`, baseXp: 30 })
     }
-
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void M.reply(
-            `🤹 Telkom Is Blocked Try Again Later 😭\n`
-        ).catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
-    }
+    run = async (M: ISimplifiedMessage): Promise<void> => void M.reply('Telkom is currently unavailable.').catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
 }
